f9853214d180987be240c49df8a30463a8f0f067
JacobLinCool <jacoblincool@gmail.com>
2023-04-21 21:24:20 +0800

    add bot author protection

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const client = new Client({
 });
 
 client.on("messageCreate", async (message) => {
+    if (message.author.bot === true) {
+        return; // 如果是機器人發出的訊息，就不要回覆（避免回覆自己）
+    }
+
     console.log(
         `在 ${message.guild.name} > ${message.channel.name} 收到來自 ${message.member.displayName} 的訊息：${message.content}`,
     );
